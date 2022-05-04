9d7ec83fcffc43cb1bce1c19be0468e7636be238
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-05-04 11:47:37 -0300

    encrypting password and making the sign in

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import express, {json} from 'express';
 import chalk from 'chalk';
 import cors from 'cors';
 import {MongoClient, ObjectId} from 'mongodb';
+import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -18,10 +19,12 @@ app.post('/sign-up', async (req, res) => {
 
     const {name, email, password} = req.body;
 
+    const encryptedPassword = bcrypt.hashSync(password, 10);
+
     const objUser = {
         name,
         email,
-        password
+        password: encryptedPassword
     };
 
     try {
@@ -36,7 +39,6 @@ app.post('/sign-up', async (req, res) => {
             return;
         }
 
-        // TODO: encrypt password
         await db.collection('users').insertOne(objUser);
         res.status(201).send('Usuário criado!');
         await mongoClient.close();
@@ -44,6 +46,42 @@ app.post('/sign-up', async (req, res) => {
     } catch (e) {
         res.status(422).send(e);
         console.log(chalk.bold.red('Erro no post sign-up', e));
+        await mongoClient.close();
+        console.log(chalk.blue('Mongo Closed'));
+    }
+    
+});
+
+app.post('/sign-in', async (req, res) => {
+
+    const {email, password} = req.body;
+
+    console.log(req.body);
+
+    try {
+        await mongoClient.connect();
+        console.log(chalk.blue('Mongo Connected'));
+        db = mongoClient.db(process.env.DATABASE_NAME);
+
+        const participantSearch = await db.collection('users').findOne({email});
+        console.log(participantSearch);
+
+        if (participantSearch && bcrypt.compareSync(password, participantSearch.password)) {
+            res.status(200).send(participantSearch);
+            console.log(participantSearch);
+            await mongoClient.close();
+            console.log(chalk.blue('Mongo Closed'));
+            return;
+        } 
+
+        res.status(404).send('Usuário não encontrado no banco de dados');
+        await mongoClient.close();
+        console.log(chalk.blue('Mongo Closed'));
+    } catch (e) {
+        res.status(422).send(e);
+        console.log(chalk.bold.red('Erro no post sign-in', e));
+        await mongoClient.close();
+        console.log(chalk.blue('Mongo Closed'));
     }
     
 })
