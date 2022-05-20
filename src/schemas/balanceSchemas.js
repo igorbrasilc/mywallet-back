0f90e0d8311065a84e1d0dbba518989615e7283c
Rodrigodcs <rodrigodcsee@gmail.com>
2022-05-20 12:07:11 -0300

    Preparando revisão de código

--- src/schemas/balanceSchemas.js
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-const transactionSchema = joi.object({
-    type: joi.string().valid('income', 'outcome').required(),
-    value: joi.number().required(),
-    description: joi.string().max(20).required()
-});
-
-export {transactionSchema};
